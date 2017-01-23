22f5bea6ec609a39d948cbf0b1c17ec8e140605d
Phelps17 <Phelps3@wisc.edu>
2017-01-23 16:58:24 -0600

    successfully logging QR code data

--- Android/app/src/main/java/com/elock/tylerphelps/elock/LockInteractionActivity.java
+++ Android/app/src/main/java/com/elock/tylerphelps/elock/LockInteractionActivity.java
@@ -2,6 +2,10 @@ package com.elock.tylerphelps.elock;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.pubnub.api.PNConfiguration;
+import com.pubnub.api.models.consumer.*;
+import com.pubnub.api.callbacks.PNCallback;
+import com.pubnub.api.PubNub;
 
 public class LockInteractionActivity extends AppCompatActivity {
 
@@ -12,4 +16,29 @@ public class LockInteractionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lock_interaction);
     }
+
+    /*
+    PNConfiguration pnConfiguration = new PNConfiguration();
+    pnConfiguration.setSubscribeKey("pub-c-a8732a53-6069-4292-8981-a1a9a230172f");
+    pnConfiguration.setPublishKey("sub-c-a27f6252-e02e-11e6-989b-02ee2ddab7fe");
+
+    pubnub = new PubNub(pnConfiguration);
+
+         pubnub.publish()
+                        .message("Unlock from Android")
+                        .channel("eLockServer")
+                        .shouldStore(true)
+                        .usePOST(true)
+                        .async(new PNCallback<PNPublishResult>() {
+                            @Override
+                            public void onResponse(PNPublishResult result, PNStatus status) {
+                                if (status.isError()) {
+                                    // something bad happened.
+                                    System.out.println("error happened while publishing: " + status.toString());
+                                } else {
+                                    System.out.println("publish worked! timetoken: " + result.getTimetoken());
+                                }
+                            }
+                        });
+     */
 }
--- Android/app/src/main/java/com/elock/tylerphelps/elock/MainActivity.java
+++ Android/app/src/main/java/com/elock/tylerphelps/elock/MainActivity.java
@@ -10,23 +10,17 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Intent;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.ArrayAdapter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-
-import com.pubnub.api.PubNub;
-import com.pubnub.api.PNConfiguration;
-import com.pubnub.api.models.consumer.*;
-import com.pubnub.api.callbacks.PNCallback;
 import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.elock.tylerphelps.elock.barcode.BarcodeCaptureActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    PubNub pubnub;
+    private static final int BARCODE_READER_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,37 +31,23 @@ public class MainActivity extends AppCompatActivity {
 
         populateListView();
 
-        PNConfiguration pnConfiguration = new PNConfiguration();
-        pnConfiguration.setSubscribeKey("pub-c-a8732a53-6069-4292-8981-a1a9a230172f");
-        pnConfiguration.setPublishKey("sub-c-a27f6252-e02e-11e6-989b-02ee2ddab7fe");
-
-        pubnub = new PubNub(pnConfiguration);
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                /*pubnub.publish()
-                        .message("Unlock from Android")
-                        .channel("eLockServer")
-                        .shouldStore(true)
-                        .usePOST(true)
-                        .async(new PNCallback<PNPublishResult>() {
-                            @Override
-                            public void onResponse(PNPublishResult result, PNStatus status) {
-                                if (status.isError()) {
-                                    // something bad happened.
-                                    System.out.println("error happened while publishing: " + status.toString());
-                                } else {
-                                    System.out.println("publish worked! timetoken: " + result.getTimetoken());
-                                }
-                            }
-                        });*/
-
                 Intent intent = new Intent(getApplicationContext(), BarcodeCaptureActivity.class);
                 startActivityForResult(intent, 1);
             }
         });
+
+        ListView listView = (ListView) findViewById(R.id.scrolling_list);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Log.e("LOCK SELECTED", ""+position);
+                }
+            }
+        );
     }
 
     @Override
@@ -93,19 +73,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //TODO handle barcode response codes
-    /*@Override
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == BARCODE_READER_REQUEST_CODE) {
             if (resultCode == CommonStatusCodes.SUCCESS) {
                 if (data != null) {
                     Barcode barcode = data.getParcelableExtra(BarcodeCaptureActivity.BarcodeObject);
                     Point[] p = barcode.cornerPoints;
-                    mResultTextView.setText(barcode.displayValue);
-                } else mResultTextView.setText(R.string.no_barcode_captured);
-            } else Log.e(LOG_TAG, String.format(getString(R.string.barcode_error_format),
-                    CommonStatusCodes.getStatusCodeString(resultCode)));
-        } else super.onActivityResult(requestCode, resultCode, data);
-    }*/
+
+                    Log.e("QR CODE SCANNER", barcode.displayValue);
+                }
+                else {
+                    Log.e("QR CODE SCANNER", "Nothing Captured");
+                }
+            }
+            else {
+                Log.e("QR CODE SCANNER", "ERROR");
+            }
+        }
+        else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
 
     public void populateListView() {
         DatabaseController dc = new DatabaseController(getApplicationContext());
